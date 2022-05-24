0e097167a4db15b71c99b9fcee8e3d752c3f94a2
Elgun Guluzada <elgunpg@code.edu.az>
2022-05-24 05:26:00 +0400

    refactor: clear code in task 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,27 +20,27 @@
 
 // Yuxarıdakı misalda c# iki listdədə var amma yeni yaranan listdə sadəcə biri olur.
 
-// let btn = document.getElementById("joinList")
-// let firstList = document.getElementById("first-list")
-// let secondList = document.getElementById("second-list")
-// let thirdDiv = document.getElementById("thirdDiv")
-// let firstListArr = firstList.innerText.split("\n")
-// let secondtListArr = secondList.innerText.split("\n")
-// let newArr = [...firstListArr, ...secondtListArr]
-// let arrList = [...new Set(newArr)]
+let btn = document.getElementById("joinList")
+let firstList = document.getElementById("first-list")
+let secondList = document.getElementById("second-list")
+let thirdDiv = document.getElementById("thirdDiv")
+let firstListArr = firstList.innerText.split("\n")
+let secondtListArr = secondList.innerText.split("\n")
+let newArr = [...firstListArr, ...secondtListArr]
+let arrList = [...new Set(newArr)]
 
-// btn.onclick = function () {
-//     let NewList = document.createElement("ul")
-//     thirdDiv.innerText = " List 3"
-//     thirdDiv.append(NewList)
-//     for (let i = 0; i < arrList.length; i++) {
-//         let newLi = document.createElement("li")
-//         NewList.append(newLi)
-//         NewList.classList.add("list-group")
-//         newLi.classList.add("list-group-item")
-//         newLi.append(arrList[i])
-//     }
-// }
+btn.onclick = function () {
+    let NewList = document.createElement("ul")
+    thirdDiv.innerText = " List 3"
+    thirdDiv.append(NewList)
+    for (let i = 0; i < arrList.length; i++) {
+        let newLi = document.createElement("li")
+        NewList.append(newLi)
+        NewList.classList.add("list-group")
+        newLi.classList.add("list-group-item")
+        newLi.append(arrList[i])
+    }
+}
 
 
 // Task 2
@@ -57,34 +57,20 @@ let firstInput = document.getElementById("first-input")
 let secondInput = document.getElementById("second-input")
 let resultInput = document.getElementById("result-input")
 
-plusBtn.onclick = function () {
-    let firstInputValue = firstInput.value;
-    let secondInputValue = secondInput.value;
 
-    let result = parseInt(firstInputValue) + parseInt(secondInputValue);
-    resultInput.value = result
+plusBtn.onclick = function () {
+    resultInput.value = parseInt(firstInput.value) + parseInt(secondInput.value);
 }
 
 minusBtn.onclick = function () {
-    let firstInputValue = firstInput.value;
-    let secondInputValue = secondInput.value;
-
-    let result = firstInputValue - secondInputValue;
-    resultInput.value = result
+    resultInput.value = firstInput.value - secondInput.value;
 }
 
 multiplyBtn.onclick = function () {
-    let firstInputValue = firstInput.value;
-    let secondInputValue = secondInput.value;
-
-    let result = firstInputValue * secondInputValue;
-    resultInput.value = result
+    resultInput.value = firstInput.value * secondInput.value;
 }
 
 divideBtn.onclick = function () {
-    let firstInputValue = firstInput.value;
-    let secondInputValue = secondInput.value;
-
-    let result = firstInputValue / secondInputValue;
-    resultInput.value = result
+    resultInput.value = firstInput.value / secondInput.value;
 }
+
